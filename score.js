9372480cae92509ad152b54fded1f2e9a492c24e
Alastair Moore <alastair.moore@protonmail.com>
2019-02-19 14:32:11 +0000

    First commit

---
+++ score.js
@@ -0,0 +1,28 @@
+const outputs = [];
+const predictionPoint = 300;
+const k = 3;
+
+
+function onScoreUpdate(dropPosition, bounciness, size, bucketLabel) {
+	outputs.push([dropPosition, bounciness, size, bucketLabel]);
+}
+
+function runAnalysis() {
+	const bucket = _.chain(outputs)
+	.map(row => [distance(row[0]), row[3]])
+	.sortBy(row => row[0])
+	.slice(0, k)
+	.countBy(row => row[1])
+	.toPairs()
+	.sortBy(row => row[1])
+	.last()
+	.first()
+	.parseInt()
+	.value();
+
+  console.log('Your point will probably fall into bucket: ' + bucket);
+}
+
+function distance(point) {
+  return Math.abs(point - predictionPoint);
+}
